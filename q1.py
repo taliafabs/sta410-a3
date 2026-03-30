d6be48cd7922efd9932bc21af0761697471662cc
taliafabs <talia.fabregas@mail.utoronto.ca>
2026-03-30 15:54:44 -0400

    3a and b implementation

--- q1.py
+++ q1.py
@@ -9,11 +9,9 @@ import matplotlib.pyplot as plt
 # random number generator
 rng = np.random.default_rng(410)
 
-
 # load the dataset, (n x p) matrix X, un-normalized
 X_data = sm.datasets.get_rdataset("mtcars").data.values
 n, p = X_data.shape[0], X_data.shape[1]
-
 
 # (a) center and scale the columns of x so that they have mean 0 and standard deviation 1
 # ---------------------------------------------------------------------------------------------------------------------
@@ -40,15 +38,14 @@ print(f"Condition numbers of the matrix X:")
 print(f"Before normalization (centering): {unnormalized_cond_num}")
 print(f"After normalization (centering): {normalized_cond_num}")
 
-# Condition numbers of R
+# Condition numbers of R (should be the same as X)
 print(f"Condition numbers of R from the QR factorization, X=QR")
 print(f"Before centering & scaling: {np.linalg.cond(R_uncentered)}")
 print(f"After centering & scaling: {np.linalg.cond(R_centered)}")
 
-
 # Plot the singular values on a log scale
 
-# un-normalized
+# before centering & scaling
 plt.figure(figsize=(8, 5))
 plt.semilogy(singular_values, marker='o', linestyle='-', markersize=3)
 plt.title('Singular values before normalization')
@@ -57,7 +54,7 @@ plt.ylabel('Singular value magnitude (log scale)')
 plt.grid(True, which="both", ls='-', alpha=0.5)
 # plt.show()
 
-# normalized
+# after centering & scaling
 plt.figure(figsize=(8, 5))
 plt.semilogy(singular_values_normalized, marker='o', linestyle='-', markersize=3)
 plt.title('Singular values after normalization')
@@ -65,17 +62,20 @@ plt.xlabel('Index')
 plt.ylabel('Singular value magnitude (log-scale)')
 plt.grid(True, which="both", ls='-', alpha=0.5)
 # plt.show()
+
+
 # ---------------------------------------------------------------------------------------------------------------------
 
 
 # (c) Randomized SVD Implementation
 # ---------------------------------------------------------------------------------------------------------------------
-def randomized_svd(X = X_normalized, r=5):
+
+def randomized_svd(X=X_normalized, r=5):
     """
     Randomized SVD Algorithm from the Assignment 3 handout, Steps 1 through 6
     """
     # step 1
-    # (I initially wrote this part iteratively & asked chat how to vectorize)
+    # (I initially wrote this part iteratively & asked chatgpt how to vectorize)
     P = rng.standard_normal(size=(p, r))
     # step 2
     Z = np.dot(X, P)
@@ -88,37 +88,39 @@ def randomized_svd(X = X_normalized, r=5):
     D_y = np.diag(svs)
     # step 6
     return Q @ U_y @ D_y @ V_y_transpose
+
+
 # ---------------------------------------------------------------------------------------------------------------------
 
 
 # (d)
 # ---------------------------------------------------------------------------------------------------------------------
-def find_smallest_ratio(X = X_normalized, threshold=0.2):
+def find_smallest_ratio(X=X_normalized, threshold=0.2):
     """
     Find the smallest ratio of r to p for which:
         ||X - Q U_y D_y V_y^T||_1 / ||X||_1 < 0.2
     where ||A||_1 is the sum of the absolute values of matrix A.
     """
-    r = 1 # start with r=1
+    r = 1  # start with r=1
     p = X.shape[1]
     norm_ratio = np.inf
     while r < p and norm_ratio >= threshold:
-        norm_ratio = np.linalg.norm((X - randomized_svd(X=X, r=r)), ord=1) /\
+        norm_ratio = np.linalg.norm((X - randomized_svd(X=X, r=r)), ord=1) / \
                      np.linalg.norm(X, ord=1)
         r += 1  # then increase it until threshold is met
     if r <= p and norm_ratio < threshold:
-        return print(f"r:{r}, p:{p}, smallest ratio of r to p:{r/p}, norm_ratio={norm_ratio},")
+        return print(f"r:{r}, p:{p}, smallest ratio of r to p:{r / p}, norm_ratio={norm_ratio},")
     else:
         return print(f"no r ≤ p achieved < {threshold}")
 
 
 print(" ")
 print(f"Part (d) output:")
-# before normalization aka centering
+# before normalization aka centering & scaling
 print(f"Without centering & scaling")
 find_smallest_ratio(X=X_data)
 print(f"With centering & scaling")
-# after normalization aka centering
+# after normalization aka centering & scaling
 find_smallest_ratio(X=X_normalized)
 
 # r_values = [1, 2, 5, 10, 15, 20, 25]
--- q3_rough.py
+++ q3_rough.py
@@ -1,13 +1,67 @@
 import numpy as np
 
+
 # TODO: 3(a) implement a function that given x returns f(x), the gradient, and the hessian
 
 def func_a(x):
+    """
+    Given a 2x1 vector x, compute the value of the Rosenbrock function f(x) = (1 - x1)^2 + (x2 - x1^2)^2,
+    its gradient vector f'(x), and its hessian matrix f''(x)
+    """
     x_1, x_2 = x[0], x[1]
     # compute f(x)
-    f_x = (1-x_1)**2 + (x_2 - x_1**2)**2
-    # compute the gradient
-    # compute the hessian
-    pass
+    f_x = (1 - x_1) ** 2 + (x_2 - x_1 ** 2) ** 2
+    # compute the gradient (2x1 vector)
+    grad = np.array([-2 * (1 - x_1) - 4 * x_1 * (x_2 - x_1 ** 2),
+                     2 * (x_2 - x_1 ** 2)])
+    # compute the Hessian (2x2 matrix)
+    hess = np.array([
+        [12 * x_1 ** 2 - 4 * x_2 + 2, -4 * x_1],
+        [-4 * x_1, 2]
+    ])
+    return f_x, grad, hess
 
-# TODO: 3(b)
+
+# TODO: 3(b) implementation so far
+
+def gradient_descent(x_0, alpha, epsilon, n_max=300):
+    """
+    Perform gradient descent with the following update rule at each iteration:
+        x_k+1 = x_k - 𝛼 • ∇f(x_k)
+
+    Terminate when || ∇f(x_k) || < ε or maximum number of iterations, nmax is reached.
+
+    Return:
+        - optimization path {x_k}
+        - convergence history {f(x_k)}
+    """
+    x_k = x_0.copy()
+    k = 0
+    optimization_path = [x_k.copy()]
+    convergence_history = [func_a(x_k)[0].copy()]
+    while k < n_max:
+        # func = func_a(x_k)
+        # f_x_k, grad_x_k = func[0], func[1]
+        # convergence_history.append(f_x_k.copy())
+        grad_x_k = func_a(x_k)[1]
+        # stop early if || ∇f(x_k) || < ε
+        if np.linalg.norm(grad_x_k) < epsilon:
+            return optimization_path, convergence_history
+        # update rule: x_k+1 = x_k - 𝛼 • ∇f(x_k)
+        x_k_1 = x_k - alpha * grad_x_k
+        x_k = x_k_1
+        # keep track of the gradient & value
+        optimization_path.append(x_k.copy())
+        convergence_history.append(func_a(x_k)[0].copy())
+        k += 1
+    return optimization_path, convergence_history
+
+# try it out???
+# how are x_0, alpha, epsilon chosen???
+# in lec 8 we leaerned that epsilon should be very small
+
+# TODO: 3(c)
+
+# def phi_alpha(alpha, x_k):
+#     grad_x_k = func_a(x_k)[1]
+#     u_k = -1 * (grad_x_k / np.linalg.norm(grad_x_k))
