4473bf3da159c2a27e1da944b552b6fe51405146
taliafabs <talia.fabregas@mail.utoronto.ca>
2026-04-02 16:05:19 -0400

    q4d implemented, still need to review

--- q4.py
+++ q4.py
@@ -41,27 +41,38 @@ def EM(x=observed_data, tolerance=1e-6):
     theta_init = 0.5
     lamb1_init = np.mean(x1)
     lamb2_init = np.mean(x2)
-    ll_init = 0     # TODO: implement log likelihood properly
-    theta, lamb1, lamb2, ll = theta_init, lamb1_init, lamb2_init, ll_init
-    log_likelihood_values = [ll_init]
+    Delta = np.where(component == 1, 1, 0)
+    # TODO: initialize the log likelihood using complete data log likelihood (i think)
+    ll_complete = np.sum(Delta * (np.log(theta_init) + np.log(lamb1_init) - lamb1_init * x) +
+                         (1 - Delta) * (np.log(1 - theta_init) + np.log(lamb2_init) - lamb2_init * x)
+                         )
+    # ll_new = np.sum(expected_membership) * np.log(theta_new) + \
+    #          np.sum(1 - expected_membership) * np.log(1 - theta_new) + \
+    #          np.sum(expected_membership * (np.log(lamb1_new) - lamb1_new * x)) + \
+    #          np.sum((1 - expected_membership) * (np.log(lamb2_new) - lamb2_new * x))
+    theta, lamb1, lamb2, ll = theta_init, lamb1_init, lamb2_init, ll_complete
+    log_likelihood_values = [ll]
     converged = False
-    while not converged:     # run algorithm until convergence
+    while not converged:  # run algorithm until convergence
         # E-step: compute E[Delta_i | X_i = x_i]
-        expected_membership = (theta * lamb1 * np.exp(- lamb2 * x)) / \
-                              (theta * lamb1 * np.exp(- lamb2 * x) + (1 - theta) * lamb1 * np.exp(- lamb2 * x))
+        expected_membership = (theta * lamb1 * np.exp(- lamb1 * x)) / \
+                              (theta * lamb1 * np.exp(- lamb1 * x) + (1 - theta) * lamb2 * np.exp(- lamb2 * x))
         # M-step: compute updated lamb1, lamb2, theta using closed-form equations
         # TODO: compute the log likelihood at each iteration?????
-        theta_new = (1/n) * np.sum(expected_membership)
+        theta_new = (1 / n) * np.sum(expected_membership)
         # TODO: derive closed form update eqns for lamb1, lamb2
         # TODO: implement updates for lamb1, lamb2
-        lamb1_new = lamb1
-        lamb2_new = lamb2
-        ll_new = 0  # TODO: compute log likelihood properly! HERE!
+        lamb1_new = np.sum(expected_membership) / np.sum(expected_membership * x)
+        lamb2_new = np.sum(1 - expected_membership) / np.sum((1 - expected_membership) * x)
+        # TODO: compute log likelihood properly! HERE! not correct yet
+        # expected log likelihood??? im pretty sure thats what this is
+        ll_new = np.sum(expected_membership) * np.log(theta_new) + \
+                 np.sum(1 - expected_membership) * np.log(1 - theta_new) + \
+                 np.sum(expected_membership * (np.log(lamb1_new) - lamb1_new * x)) + \
+                 np.sum((1 - expected_membership) * (np.log(lamb2_new) - lamb2_new * x))
         # TODO: correct convergence to be if log likelihood has converged since that's what we're maximizing
-        if np.abs(ll_new - ll) < tolerance:
+        if np.abs(ll_new - ll) < tolerance: # stop if the EM alg has converged
             converged = True
-        # if np.abs(lamb1_new - lamb1) < tolerance and np.abs(lamb2_new - lamb2) < tolerance:
-        #     converged = True
         # otherwise complete the update + keep track
         theta, lamb1, lamb2 = theta_new, lamb1_new, lamb2_new
         ll = ll_new
@@ -74,10 +85,11 @@ def EM(x=observed_data, tolerance=1e-6):
 #  how many iterations until convergence?
 # ------------------------------------------------------------------
 
-# log_likelihood = EM()
+log_likelihood = EM()
 
+# plot log likelihood at each iteration
+plt.figure(figsize=(10, 7))
 
 
 # TODO: (f)
-
-
+# -----------------------------------------
