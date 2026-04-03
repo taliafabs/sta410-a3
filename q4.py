3772d469b03dfedc4cfc64885ae912f136a4d1fe
taliafabs <talia.fabregas@mail.utoronto.ca>
2026-04-02 20:17:35 -0400

    q4 done

--- q4.py
+++ q4.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy import random
 import matplotlib.pyplot as plt
 
-rng = random.default_rng(410)
+rng = random.default_rng(123)
 
 # QUESTION 4: EM ALGORITHM FOR A MIXTURE OF EXPONENTIALS
 # ------------------------------------------------------
@@ -11,73 +11,57 @@ rng = random.default_rng(410)
 # TODO: (d) implement the EM algorithm. generate N=200 observations using true parameters
 #  lambda1=1, lambda2=5, theta=0,4
 # -------------------------------------------------------------------------------------------
-# generate data
-num_obs = 200
-true_theta = 0.4
-true_lambda1 = 1
-true_lambda2 = 5
-component = np.random.choice([1, 2], size=num_obs, p=[true_theta, 1 - true_theta])
-observed_data = np.empty(num_obs)
-observed_data[component == 1] = rng.exponential(scale=1 / true_lambda1, size=(component == 1).sum())
-observed_data[component == 2] = rng.exponential(scale=1 / true_lambda2, size=(component == 2).sum())
+# generate the data
+num_obs = 200   # N=200 observations
+true_theta, true_lambda1, true_lambda2 = 0.4, 1, 5  # true parameter values
+component = rng.binomial(1, true_theta, size=num_obs)
+observed_data = np.where(component == 1,
+                         rng.exponential(scale=1/true_lambda1, size=num_obs),
+                         rng.exponential(scale=1/true_lambda2, size=num_obs)
+                         )
 
-# plot the observed data (just for visuals!)
-plt.hist(observed_data, bins=50, density=True, alpha=0.7, color="orchid")
+# plot the observed data
+plt.hist(observed_data, bins=50, density=True, alpha=0.7, color="turquoise")
 plt.xlabel("x")
 plt.ylabel("p(x)")
 plt.title("Two-component mixture of exponential distributions")
 plt.show()
 
 
-# implement the EM algorithm
-
-def EM(x=observed_data, tolerance=1e-6):
+def EM(x=observed_data, theta_init=0.5, lamb1_init=2.0,
+       lamb2_init=3.0, tolerance=1e-12):
     """
     Expectation maximization algorithm for a mixture of two exponential components.
     """
-    # initialize the parameters
     n = len(x)
-    x1, x2 = x[component == 1], x[component == 2]
-    theta_init = 0.5
-    lamb1_init = np.mean(x1)
-    lamb2_init = np.mean(x2)
-    Delta = np.where(component == 1, 1, 0)
-    # TODO: initialize the log likelihood using complete data log likelihood (i think)
-    ll_complete = np.sum(Delta * (np.log(theta_init) + np.log(lamb1_init) - lamb1_init * x) +
-                         (1 - Delta) * (np.log(1 - theta_init) + np.log(lamb2_init) - lamb2_init * x)
-                         )
-    # ll_new = np.sum(expected_membership) * np.log(theta_new) + \
-    #          np.sum(1 - expected_membership) * np.log(1 - theta_new) + \
-    #          np.sum(expected_membership * (np.log(lamb1_new) - lamb1_new * x)) + \
-    #          np.sum((1 - expected_membership) * (np.log(lamb2_new) - lamb2_new * x))
-    theta, lamb1, lamb2, ll = theta_init, lamb1_init, lamb2_init, ll_complete
-    log_likelihood_values = [ll]
+    # initialize & keep track of log likelihood
+    ll_init = np.sum(np.log(theta_init * lamb1_init * np.exp(- lamb1_init * x) +
+                            (1 - theta_init) * lamb2_init * np.exp(- lamb2_init * x)
+                            )
+                     )
+    log_likelihood_values = [ll_init]
+    theta, lamb1, lamb2, ll = theta_init, lamb1_init, lamb2_init, ll_init
     converged = False
     while not converged:  # run algorithm until convergence
         # E-step: compute E[Delta_i | X_i = x_i]
         expected_membership = (theta * lamb1 * np.exp(- lamb1 * x)) / \
                               (theta * lamb1 * np.exp(- lamb1 * x) + (1 - theta) * lamb2 * np.exp(- lamb2 * x))
         # M-step: compute updated lamb1, lamb2, theta using closed-form equations
-        # TODO: compute the log likelihood at each iteration?????
+        # to maximize expected log likelihood
         theta_new = (1 / n) * np.sum(expected_membership)
-        # TODO: derive closed form update eqns for lamb1, lamb2
-        # TODO: implement updates for lamb1, lamb2
         lamb1_new = np.sum(expected_membership) / np.sum(expected_membership * x)
         lamb2_new = np.sum(1 - expected_membership) / np.sum((1 - expected_membership) * x)
-        # TODO: compute log likelihood properly! HERE! not correct yet
-        # expected log likelihood??? im pretty sure thats what this is
-        ll_new = np.sum(expected_membership) * np.log(theta_new) + \
-                 np.sum(1 - expected_membership) * np.log(1 - theta_new) + \
-                 np.sum(expected_membership * (np.log(lamb1_new) - lamb1_new * x)) + \
-                 np.sum((1 - expected_membership) * (np.log(lamb2_new) - lamb2_new * x))
-        # TODO: correct convergence to be if log likelihood has converged since that's what we're maximizing
-        if np.abs(ll_new - ll) < tolerance: # stop if the EM alg has converged
+        # update the observed data log likelihood
+        ll_new = np.sum(np.log(theta_new * lamb1_new * np.exp(- lamb1_new * x) +
+                               (1 - theta_new) * lamb2_new * np.exp(- lamb2_new * x)
+                               )
+                        )
+        if np.abs(ll_new - ll) < tolerance:  # stop if the EM alg has converged
             converged = True
-        # otherwise complete the update + keep track
-        theta, lamb1, lamb2 = theta_new, lamb1_new, lamb2_new
-        ll = ll_new
-        log_likelihood_values.append(ll)
-    return log_likelihood_values
+        # otherwise update params + keep track of log likelihood
+        log_likelihood_values.append(ll_new)
+        theta, lamb1, lamb2, ll = theta_new, lamb1_new, lamb2_new, ll_new
+    return lamb1, lamb2, theta, log_likelihood_values
 
 
 # TODO: (e) plot the log likelihood at each iteration of the EM alg
@@ -85,11 +69,47 @@ def EM(x=observed_data, tolerance=1e-6):
 #  how many iterations until convergence?
 # ------------------------------------------------------------------
 
-log_likelihood = EM()
+# run the EM algorithm on a 2 component mixture of exponentials
+lamb1_hat, lamb2_hat, theta_hat, log_likelihood = EM()
 
 # plot log likelihood at each iteration
 plt.figure(figsize=(10, 7))
+plt.title("Log Likelihood vs Number of EM Iterations")
+plt.xlabel("# iterations")
+plt.ylabel("Observed data log likelihood")
+plt.plot(log_likelihood, '-o', markersize=2.1, color="magenta")
+plt.show()
+
+# determine how many iterations EM required before convergence
+num_its_conv = len(log_likelihood)
+print(f"Number of EM iterations required before convergence: {num_its_conv}")
 
 
-# TODO: (f)
+# TODO: (f) Is the observed data log likelihood unimodal? Try at least 3 different starting values 
+#  and coment on whether EM runs converge to the same solution
 # -----------------------------------------
+
+# starting values that are nowhere near true values
+result1 = EM(x=observed_data, theta_init=0.95, lamb1_init=16, lamb2_init=1)
+# starting values somewhat close
+result2 = EM(x=observed_data, theta_init=0.45, lamb1_init=1.5, lamb2_init=4)
+# another arbitrarily chosen starting point
+result3 = EM(x=observed_data, theta_init=0.67, lamb1_init=6, lamb2_init=7)
+# starting point very close to true values
+result4 = EM(x=observed_data, theta_init=0.38, lamb1_init=0.98, lamb2_init=5.2)
+result5 = EM(x=observed_data, theta_init=0.1, lamb1_init=10, lamb2_init=10)
+
+print(f"EM solutions after convergence using different starting values")
+print(" ")
+print(f"starting values: lambda1=16, lambda2=1, theta=0.95")
+print(f"lambda1:{result1[0]}, lambda2: {result1[1]}, theta: {result1[2]}")
+print(" ")
+print(f"starting values: lambda1=1.5, lambda2=4, theta=0.45")
+print(f"lambda1:{result2[0]}, lambda2: {result2[1]}, theta: {result2[2]}")
+print(" ")
+print(f"starting values: lambda1=6, lambda2=7, theta=0.67")
+print(f"lambda1:{result3[0]}, lambda2: {result3[1]}, theta: {result3[2]}")
+print(" ")
+print(f"starting values: lambda1=10, lambda2=10, theta=0.1")
+print(f"lambda1:{result4[0]}, lambda2: {result4[1]}, theta: {result4[2]}")
+
--- q4_rough.py
+++ q4_rough.py
@@ -39,30 +39,82 @@ plt.show()
 
 # TODO: implement the EM alg
 
-def EM(x, tolerance=1e-6):
+def EM(x=observed_data, tolerance=1e-6):
+    """
+    Expectation maximization algorithm for a mixture of two exponential components.
+    """
     # initialize the parameters
     n = len(x)
-    theta_initial = 0.5
-    lambda1_initial = 1 / np.mean(x[components == 1])
-    lambda2_initial = 1 / np.mean(x[components == 2])
-    theta, lambda1, lambda2 = theta_initial, lambda1_initial, lambda2_initial
-    theta_history = []
-    lambda1_history = []
-    lambda2_history = []
-    while True:
-        # E-step:
-        expected_membership = (theta * lambda1 * np.exp(- lambda1 * x)) / \
-                              (theta * lambda1 * np.exp(- lambda1 * x) + (1 - theta) * lambda2 * np.exp(-lambda2 * x))
-        # M-step
-        theta_new = (1/n) * np.sum(expected_membership)
-        lambda1_new = lambda1 # TODO: fix these
-        lambda2_new = lambda2
-        converged = np.abs(lambda1_new - lambda1) < tolerance and \
-                    np.abs(lambda2_new - lambda2) < tolerance
-        if converged:
-            break
-        theta, lambda1, lambda2 = theta_new, lambda1_new, lambda2_new
-    return theta, lambda1, lambda2
+    x1, x2 = x[component == 1], x[component == 2]
+    theta_init = 0.5
+    lamb1_init = np.mean(x1)
+    lamb2_init = np.mean(x2)
+    Delta = np.where(component == 1, 1, 0)
+    # TODO: initialize the log likelihood using complete data log likelihood (i think)
+    ll_complete = np.sum(Delta * (np.log(theta_init) + np.log(lamb1_init) - lamb1_init * x) +
+                         (1 - Delta) * (np.log(1 - theta_init) + np.log(lamb2_init) - lamb2_init * x)
+                         )
+    # ll_new = np.sum(expected_membership) * np.log(theta_new) + \
+    #          np.sum(1 - expected_membership) * np.log(1 - theta_new) + \
+    #          np.sum(expected_membership * (np.log(lamb1_new) - lamb1_new * x)) + \
+    #          np.sum((1 - expected_membership) * (np.log(lamb2_new) - lamb2_new * x))
+    theta, lamb1, lamb2, ll = theta_init, lamb1_init, lamb2_init, ll_complete
+    log_likelihood_values = [ll]
+    converged = False
+    while not converged:  # run algorithm until convergence
+        # E-step: compute E[Delta_i | X_i = x_i]
+        expected_membership = (theta * lamb1 * np.exp(- lamb1 * x)) / \
+                              (theta * lamb1 * np.exp(- lamb1 * x) + (1 - theta) * lamb2 * np.exp(- lamb2 * x))
+        # M-step: compute updated lamb1, lamb2, theta using closed-form equations
+        # TODO: compute the log likelihood at each iteration?????
+        theta_new = (1 / n) * np.sum(expected_membership)
+        # TODO: derive closed form update eqns for lamb1, lamb2
+        # TODO: implement updates for lamb1, lamb2
+        lamb1_new = np.sum(expected_membership) / np.sum(expected_membership * x)
+        lamb2_new = np.sum(1 - expected_membership) / np.sum((1 - expected_membership) * x)
+        # TODO: update the observd log likelihood
+        ll_new = np.sum(np.log(theta_new * lamb1_new * np.exp(- lamb1_new * x) +
+                               (1 - theta_new) * lamb2_new * np.exp(- lamb2_new * x)
+                               )
+                        )
+        # # expected log likelihood??? im pretty sure thats what this is
+        # ll_new = np.sum(expected_membership) * np.log(theta_new) + \
+        #          np.sum(1 - expected_membership) * np.log(1 - theta_new) + \
+        #          np.sum(expected_membership * (np.log(lamb1_new) - lamb1_new * x)) + \
+        #          np.sum((1 - expected_membership) * (np.log(lamb2_new) - lamb2_new * x))
+        # TODO: correct convergence to be if log likelihood has converged since that's what we're maximizing
+        if np.abs(ll_new - ll) < tolerance: # stop if the EM alg has converged
+            converged = True
+        # otherwise complete the update + keep track
+        theta, lamb1, lamb2 = theta_new, lamb1_new, lamb2_new
+        ll = ll_new
+        log_likelihood_values.append(ll)
+    return log_likelihood_values
+
+# def EM(x, tolerance=1e-6):
+#     # initialize the parameters
+#     n = len(x)
+#     theta_initial = 0.5
+#     lambda1_initial = 1 / np.mean(x[components == 1])
+#     lambda2_initial = 1 / np.mean(x[components == 2])
+#     theta, lambda1, lambda2 = theta_initial, lambda1_initial, lambda2_initial
+#     theta_history = []
+#     lambda1_history = []
+#     lambda2_history = []
+#     while True:
+#         # E-step:
+#         expected_membership = (theta * lambda1 * np.exp(- lambda1 * x)) / \
+#                               (theta * lambda1 * np.exp(- lambda1 * x) + (1 - theta) * lambda2 * np.exp(-lambda2 * x))
+#         # M-step
+#         theta_new = (1/n) * np.sum(expected_membership)
+#         lambda1_new = lambda1 # TODO: fix these
+#         lambda2_new = lambda2
+#         converged = np.abs(lambda1_new - lambda1) < tolerance and \
+#                     np.abs(lambda2_new - lambda2) < tolerance
+#         if converged:
+#             break
+#         theta, lambda1, lambda2 = theta_new, lambda1_new, lambda2_new
+#     return theta, lambda1, lambda2
 
 # def EM(niter, x):
 #     # initialize scale params
